c00a5434fcba442eb61d8c9bce7e3cc1c8733516
taemin <jungtaemin1214@naver.com>
2025-06-30 16:11:09 +0900

    Initial commit

---
+++ src/main/java/me/jungtaemin/petboard/PetBoardApplication.java
@@ -0,0 +1,13 @@
+package me.jungtaemin.petboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBoardApplication.class, args);
+    }
+
+}
