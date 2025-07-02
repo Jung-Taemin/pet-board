1548ad03d755d6f195a245eb152ced9820f7c074
taemin <jungtaemin1214@naver.com>
2025-07-02 15:58:49 +0900

    data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO grade (grade_no, grade_name) VALUES (1, '일반회원');
-INSERT INTO grade (grade_no, grade_name) VALUES (2, '관리자');
+-- 중복시 무시 (MySQL/MariaDB 전용)
+INSERT IGNORE INTO grade (grade_no, grade_name) VALUES (1, '일반회원');
+INSERT IGNORE INTO grade (grade_no, grade_name) VALUES (2, '관리자');
